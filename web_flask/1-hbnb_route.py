e54571e938393d67e62a5b620d758bd3001c540c
soliye <soliy@gmail.com>
2022-11-13 21:02:44 -0800

    return hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Program that starts a Flask web application
+Your web application must be listening on 0.0.0.0, port 5000
+In Routes /: display “Hello HBNB!”
+/hbnb: display “HBNB”
+You must use the option strict_slashes=False in your route definition"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Display a custom String on main Route """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Display a custom message """
+    return "HBNB"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
